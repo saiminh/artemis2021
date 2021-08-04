2c93ffd95374cb048fefcd4cf1d868df738665bb
saiminh <saiminh@gmail.com>
2021-08-04 15:13:49 +0200

    company page transition in,  no scroll effect

--- js/src/company/company-after.js
+++ js/src/company/company-after.js
@@ -4,31 +4,38 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 function companyAfter(data) {
-
+  
   // Need to remove parent transforms after pagetransition to enable scroll animation:
   data.container.querySelector('.site-main').style.transform = "none";
   data.container.querySelector('.site-main > article').style.transform = "none";
-
+  let hero = data.container.querySelector('.company-hero');
+  let difference;
+  if ( hero.offsetHeight >= window.innerHeight ){
+    difference = "bottom";
+  } else {
+    difference = hero.offsetHeight + "px"
+  }
+console.log(difference);
   // Pin the Hero text
   ScrollTrigger.create({
-    trigger: data.container.querySelector('.company-hero'),
-    start: "bottom bottom",
+    trigger: hero,
+    start: "bottom " + difference,
     end: data.container.querySelector('.latest-news').offsetTop - data.container.querySelector('.company-hero').offsetHeight - 100 + "px top" ,
     pin: true,
-    pinSpacing: false
+    pinSpacing: false,
   });
   
   // Parallax scroll?
-  gsap.to(data.container.querySelector('.company-hero h1'), {
-    yPercent: -40,
-    ease: 'none',
-    scrollTrigger: {
-      trigger: data.container.querySelector('.company-manifesto'),
-      start: "top bottom",
-      scrub: .5,
-      toggleActions: 'play pause reverse pause'
-    },
-  });
+  // gsap.to(data.container.querySelector('.company-hero h1'), {
+  //   yPercent: -40,
+  //   ease: 'none',
+  //   scrollTrigger: {
+  //     trigger: data.container.querySelector('.company-manifesto'),
+  //     start: "top bottom",
+  //     scrub: .5,
+  //     toggleActions: 'play pause reverse pause'
+  //   },
+  // });
 
   
   
--- js/src/company/company-before.js
+++ js/src/company/company-before.js
@@ -5,11 +5,22 @@ gsap.registerPlugin(SplitText);
 
 function companyBefore(data){
   let splitHero = new SplitText(data.container.querySelector('.company-hero h1'), { type: "words" });
+  let splitHeroP = new SplitText(data.container.querySelector('.company-hero p'), { type: "words" });
       
   gsap.from( splitHero.words, {
     opacity: 0,
     y: -10,
     duration: 1,
+    stagger: {
+      amount: .5,
+      from: "start"
+    }
+  });
+  gsap.from( splitHeroP.words, {
+    opacity: 0,
+    y: -10,
+    duration: 1,
+    delay: .5,
     stagger: {
       amount: .5,
       from: "start"
